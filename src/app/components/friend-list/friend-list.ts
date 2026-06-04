fcbf92d29a3bf2ed32cd2dd2b4358916dfb6d226
aholiuk <agoliuk2005@gmail.com>
2026-06-04 10:41:20 +0200

    project structure created

---
+++ src/app/components/friend-list/friend-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list',
+  imports: [],
+  templateUrl: './friend-list.html',
+  styleUrl: './friend-list.scss',
+})
+export class FriendList {}
