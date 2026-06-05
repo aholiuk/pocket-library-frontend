eb7c7cd48087e00c8a740f2afe967c9ad7fe2184
aholiuk <agoliuk2005@gmail.com>
2026-06-05 10:41:59 +0200

    admin logic, html and styling added

--- src/app/components/admin/admin.ts
+++ src/app/components/admin/admin.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-admin',
   imports: [],
   templateUrl: './admin.html',
-  styleUrl: './admin.scss',
+  styleUrl: './admin.scss'
 })
-export class Admin {}
+export class Admin implements OnInit {
+
+  users: User[] = [];
+  isLoading = true;
+
+  ngOnInit(): void {
+    // dummy data — will be replaced with userService.getAll() later
+    this.users = [
+      { keycloakId: 'user-1', username: 'Anna' },
+      { keycloakId: 'user-2', username: 'Maria' },
+      { keycloakId: 'user-3', username: 'Lena' },
+      { keycloakId: 'user-4', username: 'Sophie' },
+    ];
+    this.isLoading = false;
+  }
+}
